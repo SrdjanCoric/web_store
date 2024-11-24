0787c4fbe8a33c070e4cc91262ef39cbe8486637
Srdjan Coric <srdjancoric@MacBook-Pro-2.local>
2024-11-24 23:15:11 +0100

    fix

--- lib/web_store/api.rb
+++ lib/web_store/api.rb
@@ -17,9 +17,10 @@ module WebStore
     end
 
     rescue_from ActiveRecord::RecordNotFound do |e|
-      puts "hahahahah"
-      message = e.message.gsub(/\s*\[.*\Z/, '')
-      rack_response({ status_code: 404, message: message }.to_json, 404)
+      error!({
+        status_code: 404,
+        message: "Couldn't find WebStore::Product with 'id'=#{params[:id]}"
+      }, 404)
     end
 
     before do
